f989ec6317c92909a6db4d1e8442f269e5948045
igor <152913136+totenigor@users.noreply.github.com>
2025-07-09 13:21:31 +0200

    Learning a 128x64 digital screen

---
+++ SSD1306/SSD1306/SPI.h
@@ -0,0 +1,15 @@
+/*
+ * SPI.h
+ *
+ * Created: 09/07/2025 12:19:48
+ *  Author: igwnu
+ */ 
+
+
+#ifndef SPI_H_
+#define SPI_H_
+
+extern void spi_ini(void);
+extern void spi_sendByte(char address, char digits);
+
+#endif /* SPI_H_ */
---
+++ SSD1306/SSD1306/main.c
@@ -0,0 +1,18 @@
+/*
+ * SSD1306.c
+ *
+ * Created: 09/07/2025 12:18:27
+ * Author : igwnu
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
